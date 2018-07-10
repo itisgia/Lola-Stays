821b1e6345c14620cf276ac57e84248d7d78dbdd
jiah kwon <itjiah@gmail.com>
2018-07-10 18:25:35 +1200

    js added

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
 
-//number incresemnt 
-
-var incrementPlus;
-var incrementMinus;
 var Plus  = $(".plus");
 var Minus = $(".minus");
 var incrementPlus = Plus.click(function() {
@@ -11,7 +7,12 @@ var incrementPlus = Plus.click(function() {
 		.parent(".controls-wrapper")
 		.parent(".qty")
 		.find(".quantity");
-	 $n.val(Number($n.val())+1 );
+		var amount = Number($n.val());
+		if (amount < 5 ) {
+			$n.val(Number($n.val())+1 );
+		}
+
+
 });
 
 var incrementMinus = Minus.click(function() {
@@ -26,6 +27,27 @@ var incrementMinus = Minus.click(function() {
 	}
 });
 
+
+// when the button is clicked, name and price are addded in the box
+ var btns = document.getElementsByClassName('add-btn');
+
+ $('.add-btn').click(function(e){
+	 var look = $(this)
+	 .parent(".qty")
+	 .parent(".product")
+	 .find("p");
+	 $('.foodname').text(look["0"].innerText);
+
+ });
+
+
+function addFoodList(){
+	var newLine = '<div class="orderBox">';
+			newLine += '	<p class="nameOfFood">blach blch  </p>';
+			newLine += '	<p class="orderNumber">2</p>';
+			newLine += '	<i class="fa fa-times"></i>';
+			newLine += '</div>';
+}
 //call staff
 
 $(".right").click(function(){
@@ -37,6 +59,5 @@ $(".right").click(function(){
 $(document).ready(function(){
 	$(".cart").click(function(){
 		$(".footer-pop").toggle();
-	})
+	});
 });
-
